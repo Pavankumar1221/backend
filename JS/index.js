b2fae5087a5b1185d4f77688948c4eac05c5cf28
Pedakota Pavan Kumar <pedakotapavankumar@gmail.com>
2025-07-23 00:25:00 +0530

    first commit

--- JS/index.js
+++ JS/index.js
@@ -6,3 +6,18 @@
 // understanding HTTP module
 
 
+// node and npm
+// working with node and npm
+// node inti
+// node basic usage
+
+// File Systems
+// const fs = require('fs');
+// writeFile creates new file name hey.txt and writes the content 
+// fs.writeFile("hey.txt", "Hey How are you?", function(err){
+//     if(err) console.error(err);
+//     else console.log("Done");
+// })
+
+
+// HTTP module
